ebc0f7b8e1a595fe35ba9676fe216a26d6724085
hpr-odoo <hpr@odoo.com>
2026-09-10 23:06:22 +0200

    [ADD] util/specific.py: new helper to get the cowed view ids
    - every time you update the Cowed View,
    you need to write a query to have the ids
    - after this changes, you only need to provide the key/s
    - the function will then return the IDs os the cowed view
    - you can also give the extra filters to narrow down the search results
    - ex:
    ```py
    In [7]: util.get_cowed_view_ids(cr, "website_sale.template_header_search")
    Out[7]: {4671, 5372}
    
    In [8]: util.get_cowed_view_ids(cr, "website_sale.template_header_search", "name ilike '%%header%%' AND type='qweb'")
    Out[8]: {4671, 5372}
    
    In [9]: util.get_cowed_view_ids(cr, "website_sale.template_header_search", "id = 4671")
    Out[9]: {4671}
    
    In [10]: util.get_cowed_view_ids(cr, ["website_sale.template_header_default"])
    Out[10]: {4685, 4742, 5238, 6740, 7034, 7724, 8238, 9223}
    
    In [11]: util.get_cowed_view_ids(cr, ["website_sale.template_header_search", "website_sale.template_header_default"])
    Out[11]: {4671, 4685, 4742, 5238, 5372, 6740, 7034, 7724, 8238, 9223}
    ```

--- src/util/specific.py
+++ src/util/specific.py
@@ -163,6 +163,25 @@ def rename_custom_column(cr, table_name, col_name, new_col_name, custom_module=N
     )
 
 
+def get_cowed_view_ids(cr, key, extra_where="true"):
+    # Note: When using ILIKE in `extra_where`, wrap the pattern with double percent signs (%%)
+    # Example: arch_db->>'en_US' ILIKE %%pattern%%
+
+    if isinstance(key, str):
+        key = [key]
+    cr.execute(
+        """
+        SELECT id
+          FROM ir_ui_view
+         WHERE key IN %s
+           AND website_id IS NOT NULL
+           AND ({})
+        """.format(extra_where),
+        [tuple(key)],
+    )
+    return {v_id for (v_id,) in cr.fetchall()}
+
+
 def reset_cowed_views(cr, xmlid, key=None):
     if "." not in xmlid:
         raise ValueError("Please use fully qualified name <module>.<name>")
